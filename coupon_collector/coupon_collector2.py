a8ce4f4dc464ed936ca8fdc2ef74edb2e3a9fa1e
Mateusleal2000 <mateuslg2000@gmail.com>
2023-05-07 10:25:29 -0300

    Second problem - Solving coupon collector without packs

--- coupon_collector/coupon_collector2.py
+++ coupon_collector/coupon_collector2.py
@@ -1,15 +1,39 @@
 import random
+import math
 
 
 def bernouli(p):
     if (random.random() <= p):
-        return 1
-    return 0
+        return 0
+    return 1
 
 
-def buy_coupons():
-    return 0
+def buy_coupons(n, k):
+    new_cards: int = 0
+    p = 1.0 - pow(((n - 1)/n), k)
+    for i in range(k):
+        if bernouli(p) == 0:
+            new_cards += 1
+    return new_cards
 
 
-n = 680
-k = 680
+def execute():
+    n = 680
+    k = 680
+    sd_list = []
+    mean = 0.0
+    standard_deviation = 0.0
+    for i in range(5000):
+        value = buy_coupons(n, k)
+        sd_list.append(value)
+        mean += value
+    mean /= 5000
+    for val in sd_list:
+        standard_deviation = standard_deviation + pow((val-mean), 2)
+    standard_deviation /= 5000
+    return mean, math.sqrt(standard_deviation), sd_list
+
+
+mean, standard_deviation, sd_list = execute()
+print(f"Mean of new cards: {mean}")
+print(f"Standard deviation: {standard_deviation}")
