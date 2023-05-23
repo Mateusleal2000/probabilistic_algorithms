1ebcc94e45b1252365102ee409a9fe4f68808383
Mateusleal2000 <mateuslg2000@gmail.com>
2023-05-23 20:31:32 -0300

    Initiating insertion sort

--- insertion_sort/fisher_yates.py
+++ insertion_sort/fisher_yates.py
@@ -1,15 +1,18 @@
 import numpy as np
 
-def fisher_yates_shuffle(n):
+
+def fisher_yates_shuffle(n: int):
     shuffled_list = np.random.randint(n, size=n)
-    
+
     return shuffled_list
 
+
 def insertion_sort(shuffled_list):
-    #insertion sort algorithm
+    # insertion sort algorithm
     return 0
 
-def probabilistic_insertion_sort():
-    shuffled_list = fisher_yates_shuffle()
+
+def probabilistic_insertion_sort(n: int):
+    shuffled_list = fisher_yates_shuffle(n)
     insertion_sort(shuffled_list)
     return 0
